7a2c88f214722c58b43a1b1da191ca04b0a3062b
Asad Ali <asad.ali@invozone.com>
2023-03-16 13:35:05 +0500

    All Functions Done, Awaiting for Audit

--- contracts/storageChain.sol
+++ contracts/storageChain.sol
@@ -2,23 +2,22 @@
 pragma solidity 0.8.16;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-
 contract StakingContract is Ownable {
-    mapping(address => uint256) public tokenAmount;
-    mapping(address => Stats) public NodeStats;
+    mapping(address => uint256) tokenAmount;
+    mapping(address => Stats) NodeStats;
     uint256 public adminTreasury;
     bool public paused;
 
+    // Total Number of Staked tokens of Storage chain Nodes
     uint256 public totalStakedTokens;
 
     struct Stats {
-        uint256[] validationRatio;      // storage-node did't delete any data 
-        uint256[] upTime;               // The Node was online
-        uint256[] bandWidth;            // Amount of Data that can be transfered in given amount of time
-        uint256[] storageGB;            // Amount of storage being Staked
-        bool oldUser;                   // Flag to check old/new User
+        uint256[] validationRatio; // storage-node did't delete any data
+        uint256[] upTime; // The Node was online
+        uint256[] bandWidth; // Amount of Data that can be transfered in given amount of time
+        uint256[] storageGB; // Amount of storage being Staked
+        bool oldUser; // Flag to check old/new User
     }
-
 
     // Function to Update Stats of Storage-Nodes (Admin can Access)
     function updateStats(
@@ -28,13 +27,29 @@ contract StakingContract is Ownable {
         uint256 _bandWidth,
         uint256 _storageGB
     ) external onlyOwner {
-     
         require(tokenAmount[user] > 0, "No Staked amount against this user");
         NodeStats[user].validationRatio.push(_validationRatio);
         NodeStats[user].upTime.push(_upTime);
         NodeStats[user].bandWidth.push(_bandWidth);
         NodeStats[user].storageGB.push(_storageGB);
-        emit StatsUpdated(user, _validationRatio, _upTime, _bandWidth, _storageGB); 
+        emit StatsUpdated(
+            user,
+            _validationRatio,
+            _upTime,
+            _bandWidth,
+            _storageGB
+        );
+    }
+
+    // Admin can remove Storage Node Stats when the Stake is zero
+    function deleteStats(address user) external onlyOwner {
+        require(
+            tokenAmount[user] == 0,
+            "Unstake tokens before deleting Stat's"
+        );
+        delete NodeStats[user];
+        emit StaorageNodeStatsDeleted(user); 
+
     }
 
     // Function to get Contract Address
@@ -42,13 +57,13 @@ contract StakingContract is Ownable {
         return address(this).balance;
     }
 
-    // Function for USER to Stake STOR Tokens 
+    // Function for USER to Stake STOR Tokens
     function stakeTokens() external payable {
         require(!paused, "Staking is paused");
         require(msg.value > 0, "Cannot stake 0");
         tokenAmount[_msgSender()] += msg.value;
         totalStakedTokens += msg.value;
-        NodeStats[_msgSender()].oldUser = true; 
+        NodeStats[_msgSender()].oldUser = true;
     }
 
     // The user can unstake tokens as much he has in balance
@@ -64,7 +79,7 @@ contract StakingContract is Ownable {
 
         toAccount.transfer(_tokenAmount);
         tokenAmount[_msgSender()] -= _tokenAmount;
-
+        totalStakedTokens -= _tokenAmount;
         emit TokenTransfer(_msgSender(), _tokenAmount);
     }
 
@@ -93,13 +108,11 @@ contract StakingContract is Ownable {
         emit TokenTransfer(receiverAddress, rewardAmount);
     }
 
-    // Funcction for Admin to Add more tokens in Treasury 
+    // Funcction for Admin to Add more tokens in Treasury
     function fillTreasury() external payable onlyOwner {
         require(msg.value > 0, "Invalid Amount");
         adminTreasury += msg.value;
-
     }
-
 
     // Function for Admin to Transfer tokens from treasury
     function withDrawFromTreasury(
@@ -112,8 +125,6 @@ contract StakingContract is Ownable {
         emit TokenTransfer(_receiverAddress, amount);
     }
 
-    function totalHoldings() public {}
-
     // Function to get Staked Amount of User
     function getStake(address user) external view returns (uint256) {
         return tokenAmount[user];
@@ -121,11 +132,10 @@ contract StakingContract is Ownable {
 
     // Function to get Stats of each storage Node
     function getNodeStats(address user) external view returns (Stats memory) {
-        return NodeStats[user]; 
+        return NodeStats[user];
     }
 
-
-    // FallBack Function 
+    // FallBack Function
     receive() external payable {
         emit EtherReceived(msg.sender, msg.value);
     }
@@ -134,6 +144,12 @@ contract StakingContract is Ownable {
     event FallbackTriggered(bytes data, address sender, uint256 value);
     event EtherReceived(address indexed sender, uint256 value);
     event TokenTransfer(address indexed add, uint256 value);
-    event StatsUpdated(address indexed user, uint validationRatio, uint256 upTime, uint256 bandWidth, uint256 storageGB); 
-
+    event StatsUpdated(
+        address indexed user,
+        uint256 validationRatio,
+        uint256 upTime,
+        uint256 bandWidth,
+        uint256 storageGB
+    );
+    event StaorageNodeStatsDeleted(address indexed user); 
 }
